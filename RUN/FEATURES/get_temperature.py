c4c7553e4128e0b2588987684253c0a5ddb740dd
SouRav <souravbhattacharya8159@gmail.com>
2025-05-25 15:59:06 +0530

    Update get_temperature.py

--- RUN/FEATURES/get_temperature.py
+++ RUN/FEATURES/get_temperature.py
@@ -1,22 +1,14 @@
 import requests
 
-
 def get_temperature(city):
-    # Define the URL for the weather service
     url = f"https://wttr.in/{city}?format=%t"
-    
-    # Send a request to the wttr.in service
     response = requests.get(url)
-    
-    # Check if the request was successful
+
     if response.status_code == 200:
         temperature = response.text.strip()
         temperature = temperature.replace('C','Celsius')
-        
-        
         print(f"The current temperature in {city} is {temperature}")
         return(f"The current temperature in {city} is {temperature}")
     else:
         print("Error: Could not retrieve weather data. Please check the city name.")
         return("Error: Could not retrieve weather data. Please check the city name.")
-
