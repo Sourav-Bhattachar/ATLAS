5509a0768e0ba5a9f34ea3d515b0410d3c2ff6b3
SouRav <souravbhattacharya8159@gmail.com>
2025-05-25 15:54:29 +0530

    Update youtube_video_full_screen.py

--- RUN/FEATURES/youtube_video_full_screen.py
+++ RUN/FEATURES/youtube_video_full_screen.py
@@ -4,42 +4,33 @@ import pyautogui
 
 def get_brave_window_position(query):
     brave_windows = [win for win in gw.getWindowsWithTitle('YouTube') if 'Brave' in win.title]
-    
+
     if brave_windows:
         win = brave_windows[0]
-        
-        # Check if the window is minimized
+
         if win.isMinimized:
-            win.restore()  # Restore the window to normal state
-            time.sleep(0.1)  # Wait for the window to restore
-                    
+            win.restore()
+            time.sleep(0.1)
         brave_windows[0].activate()
         time.sleep(0.2)
-        
+
         if ('full' in query and 'screen' in query) or 'maximise' in query or 'maximize' in query:
             pyautogui.press('f')
             return "Video Full Screened!"
-        
         elif 'minimize the' in query or 'minimise the' in query or 'mini the' in query:
             pyautogui.press('f')
             return "Video minimised!"
-        
         elif 'pause' in query:
             pyautogui.press('k')
             return "Video paused!"
-        
         elif "play" in query or 'resume' in query:
             pyautogui.press('k')
             return "Video played!"
-        
         elif "unmute" in query:
             pyautogui.press('m')
             return "Video unmuted!"
-        
         elif "mute" in query:
             pyautogui.press('m')
             return "Video muted!"
-    
     else:
         return "No Youtube window found in Brave browser."
-
